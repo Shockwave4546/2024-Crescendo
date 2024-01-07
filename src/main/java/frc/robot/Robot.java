f311dd72c79333ea397fcf6e468e271b40022527
Lin <linpan@Lins-MacBook-Pro.local>
2024-01-06 19:53:51 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private RobotContainer container;
+
+
+  @Override public void robotInit() {
+    container = new RobotContainer();
+  }
+
+
+  @Override public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override public void autonomousInit() {
+
+  }
+
+  @Override public void autonomousPeriodic() {
+
+  }
+
+  @Override public void teleopInit() {
+
+  }
+
+  @Override public void teleopPeriodic() {
+
+  }
+}
