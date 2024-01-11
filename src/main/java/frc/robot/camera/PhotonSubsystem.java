d5660f2c58a361a3f69d1f9248dad3ad9bc270b8
Lin Pan <linpan0328@gmail.com>
2024-01-11 00:12:00 -0500

    Stuff to do later.

---
+++ src/main/java/frc/robot/camera/PhotonSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.camera;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+// TODO: 1/11/2024 https://github.com/WHS-FRC-3467/MiniSwerve2023/blob/main/src/main/java/frc/robot/subsystems/limelight/LimelightSubsystem.java 
+public class PhotonSubsystem implements Subsystem {
+}
---
+++ src/main/java/frc/robot/camera/PoseEstimatorSubsystem.java
@@ -0,0 +1,8 @@
+package frc.robot.camera;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+// TODO: 1/11/2024 https://github.com/WHS-FRC-3467/MiniSwerve2023/blob/main/src/main/java/frc/robot/subsystems/drive/PoseEstimatorSubsystem.java
+public class PoseEstimatorSubsystem implements Subsystem {
+
+}
