4bb0d958cb86723d5da0f2d0f65aae4ff7221ba6
Lin Pan <linpan0328@gmail.com>
2024-03-08 18:28:18 -0500

    Fix.

--- src/main/java/frc/robot/auto/AutoIntakeCommand.java
+++ src/main/java/frc/robot/auto/AutoIntakeCommand.java
@@ -1,17 +1,18 @@
-package frc.robot.intakearm;
+package frc.robot.auto;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.intake.IntakeNoteCommand;
 import frc.robot.intake.IntakeSubsystem;
+import frc.robot.intakearm.IntakeArmSubsystem;
 import frc.robot.intakearm.IntakeArmSubsystem.State;
 
-public class FullIntakeCommand extends SequentialCommandGroup {
-  public FullIntakeCommand(IntakeArmSubsystem arm, IntakeSubsystem intake) {
+public class AutoIntakeCommand extends SequentialCommandGroup {
+  public AutoIntakeCommand(IntakeArmSubsystem arm, IntakeSubsystem intake) {
     addCommands(
-            new InstantCommand(() -> arm.setDesiredState(State.FLOOR), arm),
+            new InstantCommand(() -> arm.setDesiredState(State.FLOOR)),
             new IntakeNoteCommand(() -> false, intake).until(intake::hasNote),
-            new InstantCommand(() -> arm.setDesiredState(State.HOME), arm)
+            new InstantCommand(() -> arm.setDesiredState(State.HOME))
     );
 
     addRequirements(intake);
