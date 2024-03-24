157750280c5341fe9940f9cbd42f469d3e9b0293
ShockwaveComputer <dovershockwave@gmail.com>
2024-03-24 18:21:30 -0400

    BOom

--- src/main/java/org/dovershockwave/shooter/FullShootInterpolatedCommand.java
+++ src/main/java/org/dovershockwave/shooter/FullShootInterpolatedCommand.java
@@ -9,20 +9,24 @@ import org.dovershockwave.intake.IntakeSubsystem;
 import org.dovershockwave.intakearm.ArmState;
 import org.dovershockwave.intakearm.IntakeArmSubsystem;
 import org.dovershockwave.intakearm.PivotIntakeCommand;
+import org.dovershockwave.shooterwrist.ShooterWristSubsystem;
+import org.dovershockwave.shooterwrist.WristState;
 import org.dovershockwave.utils.EndActionSequentialCommandGroup;
 
 public class FullShootInterpolatedCommand extends EndActionSequentialCommandGroup {
-  public FullShootInterpolatedCommand(IntakeSubsystem intake, ShooterSubsystem shooter, IntakeArmSubsystem arm, AmpSubsystem amp) {
-    super(new ResetRobotStateCommand(shooter, intake, arm, amp));
+  public FullShootInterpolatedCommand(IntakeSubsystem intake, ShooterSubsystem shooter, IntakeArmSubsystem arm, ShooterWristSubsystem wrist) {
+    super(new ResetRobotStateCommand(shooter, intake, arm, wrist));
     addCommands(
             new PivotIntakeCommand(ArmState.HOME, arm),
             new InstantCommand(() -> shooter.rampUp(ShooterState.INTERPOLATED), shooter),
+            new InstantCommand(() -> wrist.interpolate(), wrist),
             new WaitUntilCommand(shooter::atDesiredRPS),
             new WaitCommand(0.75),
             new FeedShooterCommand(intake).withTimeout(0.25),
-            new InstantCommand(shooter::stopMotors, shooter)
+            new InstantCommand(shooter::stopMotors, shooter),
+            new InstantCommand(() -> wrist.setDesiredState(WristState.HOME), wrist)
     );
 
-    addRequirements(shooter, intake, arm, amp);
+    addRequirements(shooter, intake, arm, wrist);
   }
 }
