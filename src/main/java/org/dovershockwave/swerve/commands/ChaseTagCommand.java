0b154aa30bf5d3abdf20bb5eba7a57e63a64b56f
Lin Pan <linpan0328@gmail.com>
2024-03-24 22:49:15 -0400

    Reformat

--- src/main/java/org/dovershockwave/swerve/commands/ChaseTagCommand.java
+++ src/main/java/org/dovershockwave/swerve/commands/ChaseTagCommand.java
@@ -18,7 +18,7 @@ import org.dovershockwave.swerve.SwerveSubsystem;
 public class ChaseTagCommand extends Command {
   private static final int TAG_TO_CHASE = 1;
   private static final Transform3d TAG_TO_GOAL = new Transform3d(new Translation3d(2, 0.0, 0.0), new Rotation3d());
-  private final ProfiledPIDController omegaController = new ProfiledPIDController(0.1, 0.0, 0.0,  new TrapezoidProfile.Constraints(Swerve.MAX_ANGULAR_SPEED, 4));
+  private final ProfiledPIDController omegaController = new ProfiledPIDController(0.1, 0.0, 0.0, new TrapezoidProfile.Constraints(Swerve.MAX_ANGULAR_SPEED, 4));
   private final VisionSubsystem vision;
   private final PoseEstimatorSubsystem poseEstimator;
   private final SwerveSubsystem swerve;
@@ -41,42 +41,43 @@ public class ChaseTagCommand extends Command {
   }
 
   @Override public void execute() {
-        System.out.println(omegaController.atGoal());
+    System.out.println(omegaController.atGoal());
     if (!vision.hasViableTarget()) {
       swerve.drive(
-        -MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND),
-        -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND),
-        -MathUtil.applyDeadband(controller.getRightX(), Constants.IO.DRIVE_DEADBAND),
-        swerve.isFieldRelative(),
-        false
+              -MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND),
+              -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND),
+              -MathUtil.applyDeadband(controller.getRightX(), Constants.IO.DRIVE_DEADBAND),
+              swerve.isFieldRelative(),
+              false
       );
       return;
     }
 
-
     if (vision.hasViableTarget() && vision.getTag().getFiducialId() != TAG_TO_CHASE) return;
-    final var tagPose = vision.getTagRelativeToCenterPose();
-    final var goalPose = tagPose.transformBy(TAG_TO_GOAL).toPose2d();
-    // final var robotPose = poseEstimator.getPose2d();
+    final var tagAngle = vision.getTagRelativeToCenterPose().toPose2d().getRotation().getRadians();
     final var robotAngle = swerve.getHeadingRotation2d().getRadians();
 
-    System.out.println("1:" + goalPose.getRotation().getRadians());
-    System.out.println("2: " + robotAngle);
-    omegaController.setGoal(goalPose.getRotation().getRadians());
+    System.out.println("Tag Rad: " + tagAngle);
+    System.out.println("Robot Rad: " + robotAngle);
+    omegaController.setGoal(tagAngle);
 
-    final var rotSpeed = omegaController.atGoal() ? 0 : -omegaController.calculate(robotAngle - goalPose.getRotation().getRadians());
+    final var rotSpeed = omegaController.atGoal() ? 0 : -omegaController.calculate(robotAngle);
 
-          swerve.drive(
-        -MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND),
-        -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND),
-        -MathUtil.applyDeadband(controller.getRightX(), Constants.IO.DRIVE_DEADBAND),
-        swerve.isFieldRelative(),
-        false
-      );
+    swerve.drive(
+            -MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND),
+            -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND),
+            -MathUtil.applyDeadband(controller.getRightX(), Constants.IO.DRIVE_DEADBAND),
+            swerve.isFieldRelative(),
+            false
+    );
 
-    // swerve.drive(-MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND), 
-    // -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND), 
-    //   -rotSpeed, true, false);
+//    swerve.drive(
+//            -MathUtil.applyDeadband(controller.getLeftY(), Constants.IO.DRIVE_DEADBAND),
+//            -MathUtil.applyDeadband(controller.getLeftX(), Constants.IO.DRIVE_DEADBAND),
+//            -rotSpeed,
+//            swerve.isFieldRelative(),
+//            false
+//    );
   }
 
   @Override public void end(boolean interrupted) {
